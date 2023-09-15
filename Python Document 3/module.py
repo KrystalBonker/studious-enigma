c0b60529a54a2af136afc2ad2df791468b28951d
KrystalBonker <134010349+KrystalBonker@users.noreply.github.com>
2023-09-15 11:05:42 -0400

    Add files via upload

---
+++ Python Document 3/module.py
@@ -0,0 +1,14 @@
+
+import math
+
+
+def printName(name):
+    print(f"Hello Mr/Ms. {name}... we have been wating for you!") 
+
+def squareFeet(length,width):
+    area = length * width
+    print(area, "is the square footage of this house.")
+
+def circleA(radius):
+    c = 2 * math.pi * radius
+    print(c, "is the circumference of this circle.")
